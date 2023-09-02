662ff3ef7445c88c37edcd692418734bbce321aa
ravikt <124181635+ravivarmakt@users.noreply.github.com>
2023-09-02 11:16:54 +0530

    Add files via upload

---
+++ Untitled aug 30.c
@@ -0,0 +1,16 @@
+//write a programme to find factorial of a number using while lopp//
+#include<stdio.h>
+main()
+{
+	int num;
+	printf("enter the value of number\n");
+	scanf("%d",&num);
+	int i=1;
+	while(i<=num)
+	{
+		fact=fact*i;
+		i++;
+	}
+	printf("factorial is %d ",fact)
+	
+}
